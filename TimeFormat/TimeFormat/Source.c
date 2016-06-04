571ee72d2fe3c458a9066d33d2f2aef87b7c1fb2
Liafell <leneli.lehmann@yahoo.de>
2016-06-04 17:40:05 +0300

    TimeFormat

--- TimeFormat/TimeFormat/Source.c
+++ TimeFormat/TimeFormat/Source.c
@@ -6,16 +6,42 @@
 int main(void) {
 	setlocale(LC_ALL, "Rus");
 
-	int minutes;
+	int minutes, hours;
 
 	printf("Введите количество минут:\t");
 	scanf_s("%d", &minutes);
 
 	printf("------------------------------\n");
 	printf("ЗАДАННОЕ ВРЕМЯ\n");
-	printf("%d\n", minutes);
+
+	if (minutes < 60) {
+		hours = 0;
+	}
+	else {
+		hours = (minutes - (minutes % 60)) / 60;
+		minutes = minutes % 60;
+	}
+
+	if (hours < 10 || minutes < 10) {
+		if (hours < 10 && minutes < 10) {
+			printf("0%d:0%d\n", hours, minutes);
+		}
+		else {
+			if (hours < 10) {
+				printf("0%d:%d\n", hours, minutes);
+			}
+			if (minutes < 10) {
+				printf("%d:0%d\n", hours, minutes);
+			}
+		}
+	}
+	else {
+		printf("%d:%d\n", hours, minutes);
+	}
+
 	printf("------------------------------\n");
 
 	system("pause");
 	return 0;
 }
+
